4dfcbc89f59276e2dd79d23fc6737b1f5cc5de27
Romain Endelin <romain@endelin.fr>
2016-07-21 17:38:21 +0200

    Fix strange error with CORS

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module ProductManagerAssessment
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
